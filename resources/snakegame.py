9e443a79736b10d87d44aa32072de1f029d89980
Mahesh Kumar Patel <88634899+ksmaheshpatel@users.noreply.github.com>
2022-06-08 20:35:43 +0200

    Update snakegame.py

--- resources/snakegame.py
+++ resources/snakegame.py
@@ -1,1 +1,23 @@
+import pygame
+from pygame.locals import *
+import time
+import random
+
+SIZE = 40
+BACKGROUND_COLOR = (110, 110, 5)
+
+class Redio:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("resources/redio.jpg").convert()
+        self.x = 120
+        self.y = 120
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(1,24)*SIZE
+        self.y = random.randint(1,19)*SIZE
 
